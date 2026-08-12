73db3eeda618a6a8b38d97d25e2ce63ace001091
ahmeddwieb1 <ahmeddwieb713@gmail.com>
2026-08-12 11:19:41 +0300

    improve gitlab and docker compose to make intgration good with frontend

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "wasaya_role" {
+  name = "wasaya_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "s3_policy" {
   name = "test_policy"
   role = aws_iam_role.wasaya_role.id
@@ -21,23 +38,6 @@ resource "aws_iam_role_policy" "s3_policy" {
     }
   ]
 })
-}
-
-resource "aws_iam_role" "wasaya_role" {
-  name = "wasaya_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
 }
 
 resource "aws_iam_role_policy" "rds_policy" {
@@ -78,4 +78,9 @@ resource "aws_iam_role_policy" "secretsmanager_policy" {
     }
   ]
 })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.wasaya_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }

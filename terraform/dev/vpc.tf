c13d50e76b459fe793b6539eb95f033f738046a0
ahmeddwieb1 <ahmeddwieb713@gmail.com>
2026-08-31 02:00:19 +0300

    add dev stage without gitlab

---
+++ terraform/dev/vpc.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_vpc" "mainVPC" {
+  cidr_block = "11.0.0.0/16"
+}
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.mainVPC.id
+  cidr_block = "11.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-1a"
+}
+resource "aws_internet_gateway" "mainIGW" {
+  vpc_id = aws_vpc.mainVPC.id
+}
